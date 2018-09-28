43c623298812f3763a3a8e4d42b3e1e1209c44db
Romain Dortier <romain.dortier@gmail.com>
2018-09-28 16:37:15 +0200

    Update index.php

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@
 # Please see LICENSE.txt for applicable license terms and NOTICE.txt for applicable notices. 
 */
 
-$AppName = "Demo Web App";
+$AppName = "Demo Web App (modif)";
 
 ob_start();
 
